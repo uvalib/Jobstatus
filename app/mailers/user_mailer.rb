10527ef50a229e6953da28673b855f8be5188d37
xw5d <xw5d@d-172-27-72-154.dhcp.virginia.edu>
2018-05-17 13:00:15 -0400

    DCMD-359: clean up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,6 @@ class UserMailer < ApplicationMailer
   def jobstatus_email
     @url = []
     Job.all_statuses.each{|k,v| @url << (ENV['BASE_URL'].nil? ? 'http://localhost:3000': ENV['BASE_URL'])+'/jobs/group?name='+ k if v == false }
-    @url.inspect
     mail(to: EMAIL_TO, subject: 'Job Status: There is a failed job')
   end
 end
