0f823a50c54d3c46e4ff5f2fab91b1e8046d77da
xw5d <xw5d@d-172-27-72-154.dhcp.virginia.edu>
2018-05-17 12:29:20 -0400

    DCMD-359: Use system variable of BASE_URL in email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,8 @@ class UserMailer < ApplicationMailer
   default from:  EMAIL_FROM
   def jobstatus_email
     @url = []
-    Job.all_statuses.each{|k,v| @url << BASE_URL+'/jobs/group?name='+ k if v == false }
+    Job.all_statuses.each{|k,v| @url << (ENV['BASE_URL'].nil? ? 'http://localhost:3000': ENV['BASE_URL'])+'/jobs/group?name='+ k if v == false }
+    @url.inspect
     mail(to: EMAIL_TO, subject: 'Job Status: There is a failed job')
   end
 end
