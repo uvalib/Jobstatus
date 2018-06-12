37936051ac62b228fe279936afb7e6f64fa30fe3
xw5d <xw5d@d-172-25-10-223.dhcp.virginia.edu>
2018-06-12 13:51:39 -0400

    Change email sender and receiver

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
-  default from:  EMAIL_FROM
+  default from:  ENV['SENDER']
   def jobstatus_email
     @url = []
     Job.all_statuses.each{|k,v| @url << (ENV['BASE_URL'].nil? ? 'http://localhost:3000': ENV['BASE_URL'])+'/jobs/group?name='+ k if v == false }
-    mail(to: EMAIL_TO, subject: 'Job Status: There is a failed job')
+    mail(to: ENV['RECEIVER'], subject: 'Job Status: There is a failed job')
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,9 +52,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  #EMAIL
-  EMAIL_FROM = ENV['SENDER']
-  EMAIL_TO = ENV['RECEIVER']
-
 end
 
