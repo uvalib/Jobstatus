c8a139b76d65210e23821a024471e1a32b743182
xw5d <xw5d@d-172-25-207-63.dhcp.virginia.edu>
2018-08-29 16:49:04 -0400

    DCMD-513: fix Email notification on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+
+  config.action_mailer.smtp_settings = {
+      address:              ENV["SMTP_HOST"]
+  }
+
 end
